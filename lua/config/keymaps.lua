29174e3ea24a3973c224d4943a14285bb44ffcc5
Slamet Nugroho <67224487+earthly-pixel@users.noreply.github.com>
2026-07-16 15:09:13 +0700

    Enhance keymaps with window resizing and LSP navigation
    Added key mappings for resizing windows and navigating to definitions in new tabs.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = " "
 local map = vim.keymap.set
 
 -- Clear search highlights on pressing Escape
-map("n", "<jk>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
 
 -- Better window navigation
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
@@ -17,5 +17,15 @@ map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
 
--- Exit insert mode by pressing 'jk'
+-- Resize windows with arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Map jk as Escape in insert mode
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+-- Go to definition in new tab (Native LSP)
+map("n", "<leader>gt", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition in new tab" })
+map("n", "<leader>gv", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition in vertical split" })
