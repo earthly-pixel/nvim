fc4063ab3e5b2f3f26e01b9c0b1ff03e95c49c05
Slamet Nugroho <67224487+earthly-pixel@users.noreply.github.com>
2026-07-16 15:13:19 +0700

    Register Blade filetype and external parser
    Added support for Blade filetype and custom parser.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,33 @@ return {
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    -- Old (Remove this):
-    -- require("nvim-treesitter.configs").setup({ ... })
+    -- Register the blade filetypr so treesitter can parse it
+    vim.filetype.add({
+      pattern = {
+        ["%.blade%.php"] = "blade",
+      },
+    })
+
+    -- Register the custom external treesitter parser for blade
+    local register_blade = function ()
+      require("nvim-treesitter.parsers").blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade", -- local path or git repo
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        tier = 2, -- WARN: `tier 2` is required for external parsers, otherwise treesitter will not load it
+        filetype = "blade", -- if filetype does not match the parser name
+      }
+    end
+
+    -- Register immediately so that treesitter can parse blade files
+    register_blade()
+    -- .. and register again after treesitter is loaded, in case it was loaded before this plugin
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TSUpdate",
+      callback = register_blade,
+    })
 
     -- New API:
     local ts = require("nvim-treesitter")
@@ -14,7 +39,8 @@ return {
     
     -- Install parsers explicitly:
     ts.install({
-      "bash", "c", "html", "javascript", "json", "lua", "markdown", "python", "query", "typescript", "vim", "vimdoc", "yaml"
+      "bash", "c", "html", "javascript", "json", "lua", "markdown",
+      "php", "blade", "python", "query", "typescript", "vim", "vimdoc", "yaml"
     })
 
     -- Highlighting is native to Neovim 0.12 by default, but to ensure 
