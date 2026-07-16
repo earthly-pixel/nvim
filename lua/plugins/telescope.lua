49f3609cb48963ca9c645f446abb6c679b5df249
Slamet Nugroho <67224487+earthly-pixel@users.noreply.github.com>
2026-07-16 15:12:11 +0700

    Add LSP and Git commands to Telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,10 @@ return {
       { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep (Search Text)" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Open Buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+      { "<leader>gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Find LSP Definitions" },
+      { "<leader>gr", "<cmd>Telescope lsp_references<cr>", desc = "LSP References" },
+      { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
+      { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git Commits" },
     },
   },
 }
