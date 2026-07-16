4b5976624c715a97e70d37622d8e7604f746253d
Angga Septiaji <angga@debian12.magelangkota.go.id>
2026-07-16 13:44:44 +0700

    update setting for debian 13

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -35,7 +35,7 @@ return {
           auto_trigger = true,
           debounce = 75,
           keymap = {
-            accept = "<Tab>",
+            accept = "<M-l>",
             next = "<M-]>",
             prev = "<M-[>",
             dismiss = "<C-]>",
